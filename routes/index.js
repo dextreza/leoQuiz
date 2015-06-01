a4d6f8963cf012e8477ea9fc09b02a5dd3b80f0b
dextreza <gloyle@gmail.com>
2015-06-01 23:49:13 +0200

    mod6-6 marco
    Añadida una pregunta ya con un marco general de pagina

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var quizControler = require('../controllers/quiz_controllers.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'LeoQuiz' });
 });
+
+router.get('/quizes/question',quizControler.question);
+router.get('/quizes/answer',quizControler.answer);
 
 module.exports = router;
